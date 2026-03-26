24bcfdfcdf12c961f70cc077fdee0272d2f5c730
Rua Fuller <ruafuller@gmail.com>
2026-03-26 08:32:02 +0000

    Implement framework for default maps

--- src/lib.rs
+++ src/lib.rs
@@ -38,3 +38,125 @@ pub mod province {
         }
     }
 }
+
+pub mod default {
+    use std::collections::HashMap;
+
+    use crate::province::*;
+
+    #[derive(Clone, PartialEq, Eq, Debug)]
+    pub struct DefaultProvince {
+        /// The full name of the province.
+        name: &'static str,
+        /// Whether the province is a supply centre.
+        is_centre: bool,
+        /// Whether an army can move to the province.
+        is_land: bool,
+        /// Whether a fleet can convoy from the province.
+        is_water: bool,
+        /// A list containing all coasts of the province.
+        coasts: Vec<Coast>,
+        /// A HashMap that returns whether the province is adjacent for Armies and Fleets, respectively.
+        adjacencies: HashMap<&'static str, (bool, bool)>,
+    }
+
+    impl DefaultProvince {
+        pub fn new(
+            name: &'static str,
+            is_centre: bool,
+            is_land: bool,
+            is_water: bool,
+            coasts: Vec<Coast>,
+            input_adjacencies: Vec<(&'static str, bool, bool)>,
+        ) -> Self {
+            let mut adjacencies = HashMap::with_capacity(input_adjacencies.len());
+            for (name, army, fleet) in input_adjacencies {
+                adjacencies.insert(name, (army, fleet));
+            }
+            DefaultProvince {
+                name,
+                is_centre,
+                is_land,
+                is_water,
+                coasts,
+                adjacencies,
+            }
+        }
+
+        pub fn romans() -> (
+            DefaultProvince,
+            DefaultProvince,
+            DefaultProvince,
+            DefaultProvince,
+        ) {
+            (
+                DefaultProvince::new(
+                    "Cato",
+                    true,
+                    true,
+                    false,
+                    Vec::new(),
+                    vec![("Caesar", true, false), ("Brutus", true, false)],
+                ),
+                DefaultProvince::new(
+                    "Caesar",
+                    false,
+                    true,
+                    false,
+                    Vec::new(),
+                    vec![
+                        ("Cato", true, false),
+                        ("Brutus", true, false),
+                        ("Pompey", true, false),
+                    ],
+                ),
+                DefaultProvince::new(
+                    "Brutus",
+                    false,
+                    true,
+                    false,
+                    Vec::new(),
+                    vec![
+                        ("Cato", true, false),
+                        ("Brutus", true, false),
+                        ("Pompey", true, false),
+                    ],
+                ),
+                DefaultProvince::new(
+                    "Pompey",
+                    true,
+                    true,
+                    false,
+                    Vec::new(),
+                    vec![("Caesar", true, false), ("Brutus", true, false)],
+                ),
+            )
+        }
+    }
+
+    impl Province for DefaultProvince {
+        fn id(&self) -> &str {
+            self.name
+        }
+        fn is_centre(&self) -> bool {
+            self.is_centre
+        }
+        fn is_land(&self) -> bool {
+            self.is_land
+        }
+        fn is_water(&self) -> bool {
+            self.is_water
+        }
+        fn get_coasts(&self) -> &Vec<Coast> {
+            &self.coasts
+        }
+
+        fn is_adjacent_to(&self, other: &Self, unit: Unit) -> bool {
+            let &(army, fleet) = self.adjacencies.get(other.name).unwrap_or(&(false, false));
+            match unit {
+                Unit::Army => army,
+                Unit::Fleet => fleet,
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,18 @@
 use std::collections::HashMap;
 
+use diplo_rs::default;
 use diplo_rs::province::*;
 
 fn main() {
-    let cat = LocalProvince::new(
-        "Cato",
-        true,
-        true,
-        false,
-        Vec::new(),
-        vec![("Caesar", true, false), ("Brutus", true, false)],
-    );
-    let cae = LocalProvince::new(
-        "Caesar",
-        false,
-        true,
-        false,
-        Vec::new(),
-        vec![
-            ("Cato", true, false),
-            ("Brutus", true, false),
-            ("Pompey", true, false),
-        ],
-    );
-    let bru = LocalProvince::new(
-        "Brutus",
-        false,
-        true,
-        false,
-        Vec::new(),
-        vec![
-            ("Cato", true, false),
-            ("Brutus", true, false),
-            ("Pompey", true, false),
-        ],
-    );
-    let pom = LocalProvince::new(
-        "Pompey",
-        true,
-        true,
-        false,
-        Vec::new(),
-        vec![("Caesar", true, false), ("Brutus", true, false)],
-    );
+    let (cat, cae, bru, pom) = default::DefaultProvince::romans();
 
     for unit in [Unit::Army, Unit::Fleet] {
-        for x in [&cat, &cae, &pom, &bru] {
-            for y in [&cat, &cae, &pom, &bru] {
+        for x in [&cat, &cae, &bru, &pom] {
+            for y in [&cat, &cae, &bru, &pom] {
                 if x.is_adjacent_to(&y, unit) {
-                    println!("{:?} in {} is adjacent to {}", unit, x.name, y.name,);
+                    println!("{:?} in {} is adjacent to {}", unit, x.id(), y.id());
                 }
             }
-        }
-    }
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-struct LocalProvince {
-    /// The full name of the province.
-    name: &'static str,
-    /// Whether the province is a supply centre.
-    is_centre: bool,
-    /// Whether an army can move to the province.
-    is_land: bool,
-    /// Whether a fleet can convoy from the province.
-    is_water: bool,
-    /// A list containing all coasts of the province.
-    coasts: Vec<Coast>,
-    /// A HashMap that returns whether the province is adjacent for Armies and Fleets, respectively.
-    adjacencies: HashMap<&'static str, (bool, bool)>,
-}
-
-impl LocalProvince {
-    fn new(
-        name: &'static str,
-        is_centre: bool,
-        is_land: bool,
-        is_water: bool,
-        coasts: Vec<Coast>,
-        input_adjacencies: Vec<(&'static str, bool, bool)>,
-    ) -> LocalProvince {
-        let mut adjacencies = HashMap::with_capacity(input_adjacencies.len());
-        for (name, army, fleet) in input_adjacencies {
-            adjacencies.insert(name, (army, fleet));
-        }
-        LocalProvince {
-            name,
-            is_centre,
-            is_land,
-            is_water,
-            coasts,
-            adjacencies,
-        }
-    }
-}
-
-impl Province for LocalProvince {
-    fn id(&self) -> &str {
-        self.name
-    }
-    fn is_centre(&self) -> bool {
-        self.is_centre
-    }
-    fn is_land(&self) -> bool {
-        self.is_land
-    }
-    fn is_water(&self) -> bool {
-        self.is_water
-    }
-    fn get_coasts(&self) -> &Vec<Coast> {
-        &self.coasts
-    }
-
-    fn is_adjacent_to(&self, other: &Self, unit: Unit) -> bool {
-        let &(army, fleet) = self.adjacencies.get(other.name).unwrap_or(&(false, false));
-        match unit {
-            Unit::Army => army,
-            Unit::Fleet => fleet,
         }
     }
 }
