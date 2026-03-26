f20e32efe117ad1576368a652a1485f7e4999268
Rua Fuller <ruafuller@gmail.com>
2026-03-26 13:37:23 +0000

    Store provinces in an array

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use diplo_rs::default;
 use diplo_rs::province::*;
 
 fn main() {
-    let [cat, cae, bru, pom] = default::DefaultProvince::romans();
+    let romans = default::DefaultProvince::romans();
 
     for unit in [Unit::Army, Unit::Fleet] {
-        for x in [&cat, &cae, &bru, &pom] {
-            for y in [&cat, &cae, &bru, &pom] {
+        for x in &romans {
+            for y in &romans {
                 if x.is_adjacent_to(&y, unit) {
                     println!("{:?} in {} is adjacent to {}", unit, x.id(), y.id());
                 }
